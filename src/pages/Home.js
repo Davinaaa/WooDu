1b7da4e39746ccb8ef50a08c1b1364c1b810b10f
Ziyunp <ziyun_poh@hotmail.com>
2022-11-08 23:12:16 +0000

    add all collections

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,17 @@
-import logo from '../logo.svg';
+import WooDuLogo from "../WooDuLogo.png";
 
-
-import './Home.css';
+import "./Home.css";
 
 function renderCollections() {
   const collections = [];
-  for(let i = 0; i < 12; i++) {
+  for (let i = 1; i < 110; i++) {
     collections.push(
-      <img className="collectionItem" src={require(`../collection/woodu-${i}.png`)} alt={`woodu collection ${i}`} />
-    )
+      <img
+        className="collectionItem"
+        src={require(`../collection/WooDu${i}.png`)}
+        alt={`woodu collection ${i}`}
+      />
+    );
   }
   return collections;
 }
@@ -16,16 +19,14 @@ function Home() {
   return (
     <div className="home">
       <div className="logoContainer">
-        <img className="logo" src={logo} alt="woodu logo"/>
+        <img className="logo" src={WooDuLogo} alt="woodu logo" />
       </div>
       <div className="welcomeText">
         Browse from our
-        <br/>
+        <br />
         100+ Collections
       </div>
-      <div className="allCollections">
-        {renderCollections()}
-      </div>
+      <div className="allCollections">{renderCollections()}</div>
     </div>
   );
 }
